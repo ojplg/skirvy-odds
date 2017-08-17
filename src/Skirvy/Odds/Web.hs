b056855e110f2efe493c6f9e991ab73a06ab0ccb
ojplg <lees@ripco.com>
2017-08-17 14:56:42 -0500

    Start reading the form

--- src/Skirvy/Odds/Web.hs
+++ src/Skirvy/Odds/Web.hs
@@ -3,12 +3,21 @@ module Skirvy.Odds.Web
     ) where
 
 import Control.Monad (msum)
-import Happstack.Server (nullConf, simpleHTTP, ok, dir, nullDir, 
-                         serveDirectory, Browsing (DisableBrowsing),
-                         toResponse, serveFile, asContentType)
+import Happstack.Server (nullConf, simpleHTTP, ok, dir, nullDir, look,
+                         serveDirectory, Browsing (DisableBrowsing), Response,
+                         toResponse, serveFile, asContentType, ServerPart)
 
 handleRequest :: IO ()
 handleRequest = simpleHTTP nullConf $ msum 
                   [ dir "foo" $ ok $ toResponse "Foo" , 
+                    dir "calculate" $ handleCalculateRequest,
                     dir "static" $ serveDirectory DisableBrowsing ["index.html"] "./web-assets/" ,
                     nullDir >> serveFile (asContentType "text/html") "./web-assets/index.html" ]
+
+handleCalculateRequest :: ServerPart Response
+handleCalculateRequest =
+  do attacker <- look "attacker"
+     defender <- look "defender"
+     ok $ toResponse $ "Trying to calculate for attacker: " ++ attacker ++ 
+          " and defender " ++ defender
+
