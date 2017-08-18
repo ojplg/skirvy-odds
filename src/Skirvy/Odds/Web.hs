0e5f3f57a919c91b10859bf6a2565dfe3e55e07e
ojplg <lees@ripco.com>
2017-08-17 21:48:43 -0500

    Parse out integers from calculation submission

--- src/Skirvy/Odds/Web.hs
+++ src/Skirvy/Odds/Web.hs
@@ -2,22 +2,28 @@ module Skirvy.Odds.Web
     ( handleRequest
     ) where
 
-import Control.Monad (msum)
+import Control.Monad (msum, liftM)
 import Happstack.Server (nullConf, simpleHTTP, ok, dir, nullDir, look,
                          serveDirectory, Browsing (DisableBrowsing), Response,
-                         toResponse, serveFile, asContentType, ServerPart)
+                         toResponse, serveFile, asContentType, ServerPart,
+                         HasRqData)
 
 handleRequest :: IO ()
 handleRequest = simpleHTTP nullConf $ msum 
-                  [ dir "foo" $ ok $ toResponse "Foo" , 
-                    dir "calculate" $ handleCalculateRequest,
+                  [ dir "calculate" $ handleCalculateRequest,
                     dir "static" $ serveDirectory DisableBrowsing ["index.html"] "./web-assets/" ,
                     nullDir >> serveFile (asContentType "text/html") "./web-assets/index.html" ]
 
 handleCalculateRequest :: ServerPart Response
 handleCalculateRequest =
-  do attacker <- look "attacker"
-     defender <- look "defender"
-     ok $ toResponse $ "Trying to calculate for attacker: " ++ attacker ++ 
-          " and defender " ++ defender
+  do attacker <- lookInt "attacker"
+     defender <- lookInt "defender"
+     ok $ toResponse $ doCalculation attacker defender
 
+lookInt :: (HasRqData m, Monad m) => String -> m Int
+lookInt = liftM read . look
+
+doCalculation :: Int -> Int -> String
+doCalculation attacker defender = 
+    "Trying to calculate for attacker " ++ (show attacker)
+       ++ " and defender " ++ (show defender)
