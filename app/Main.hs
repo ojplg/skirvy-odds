0cb7e4c100fd591ec245336871d6408855b1d0ff
ojplg <lees@ripco.com>
2017-08-21 16:10:56 -0500

    Print out more directory stuff

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,11 @@ import System.Directory
 
 main :: IO ()
 main = do putStrLn "Starting ... " 
-          stuff <- listDirectory "."
-          mapM_ putStrLn stuff
+          dotStuff <- listDirectory "."
+          putStrLn "Contents of ."
+          mapM_ putStrLn dotStuff
+          tmpStuff <- listDirectory "/tmp"
+          putStrLn "--------------------"
+          putStrLn "Contents of tmp"
+          mapM_ putStrLn tmpStuff
           handleRequest
