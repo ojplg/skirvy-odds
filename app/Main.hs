4e0fa308b2977db07ee51da7596ea514d6b65965
ojplg <lees@ripco.com>
2017-08-21 19:14:39 -0500

    Get docker build working

--- app/Main.hs
+++ app/Main.hs
@@ -4,16 +4,4 @@ import Skirvy.Odds.Web (handleRequest)
 import System.Directory
 
 main :: IO ()
-main = do putStrLn "Starting ... " 
-          dotStuff <- listDirectory "."
-          putStrLn "Contents of ."
-          mapM_ putStrLn dotStuff
-          tmpStuff <- listDirectory "/tmp"
-          putStrLn "--------------------"
-          putStrLn "Contents of tmp"
-          mapM_ putStrLn tmpStuff
---          webStuff <- listDirectory "/tmp/web-assets"
---          putStrLn "--------------------"
---          putStrLn "Contents of web"
---          mapM_ putStrLn webStuff
-          handleRequest
+main = putStrLn "Starting ... " >> handleRequest
---
+++ runDckrImg.sh
@@ -0,0 +1,1 @@
+docker run --name skirvy-odds-server -p 8080:8080 skirvy-odds-server
