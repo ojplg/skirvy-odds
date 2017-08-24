5519cf9d35a5eb7805758ad1d3d8903945ede97b
ojplg <lees@ripco.com>
2017-08-24 10:00:15 -0500

    Add test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
+import Test.QuickCheck (quickCheck)
+import Skirvy.Odds.Calculator (partitionedOutcomes)
+import Data.Map.Lazy as M (elems)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = quickCheck testResultsAddToOne
+
+testResultsAddToOne :: (Int, Int) -> Bool
+testResultsAddToOne (a,d) = (abs $ 1.0 - attPer - defPer) < 0.0001
+  where (attMap, defMap) = partitionedOutcomes attCnt defCnt
+        attPer = sum $ M.elems attMap
+        defPer = sum $ M.elems defMap
+        attCnt = ( a `mod` 250 ) + 2
+        defCnt = ( d `mod` 250 ) + 1
+
